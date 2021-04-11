34fc3dabdf2da8942ca203e2866e0e249e48e456
musosy <musosy@users.noreply.github.com>
2021-04-11 19:23:40 +0200

    autoload

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Wcs\Hello;
+
+$hello = new Hello();
+$hello->talk();
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Wcs;
+
+class Hello{
+    public function talk()
+    {
+        var_dump("Hello World");
+        return "Hello World";
+    }
+};
